009eb9f94522a4cc06fa141b3dc31e0a249eab41
Danilo <dabio@users.noreply.github.com>
2018-09-20 16:24:06 +0200

    Store notification items

--- notification/app.py
+++ notification/app.py
@@ -1,12 +1,52 @@
+import boto3
 import json
 import logging
+import uuid
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+TOPIC = 'macnerd-topic'
+ITEM = 'macnerd-item'
+
+
+def convert(body):
+    return {
+        'id': str(uuid.uuid5(uuid.NAMESPACE_DNS, body.get('id'))),
+        'topic_id': body.get('topic_id'),
+        'title': body.get('title'),
+        'summary': body.get('summary'),
+        'content': body.get('content'),
+        'link': body.get('permalinkUrl'),
+        'published': body.get('published'),
+        'updated': body.get('updated'),
+    }
+
+
+def respond(code=200):
+    return {
+        'statusCode': code
+    }
+
 
 def handler(event, context):
-    logging.info(json.dumps(json.loads(event.get("body")), indent=4))
-    return {
-        'statusCode': 200
-    }
+    logging.info(event)
+
+    resource = boto3.resource('dynamodb')
+    table = resource.Table(TOPIC)
+
+    topic = table.get_item(Key={'id': event.get('pathParameters').get('id')})
+    logging.info(topic)
+    if not topic.get('Item'):
+        return respond(404)
+
+    body = json.loads(event.get("body"))
+    logging.info(json.dumps(body, indent=4))
+
+    table = resource.Table(ITEM)
+    with table.batch_writer() as batch:
+        for item in body.get('items', []):
+            item['topic_id'] = topic.get('Item').get('id')
+            batch.put_item(Item=convert(item))
+
+    return respond(200)
