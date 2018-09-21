ab0ada71154a52c208e011923b935594603ac871
Danilo <dabio@users.noreply.github.com>
2018-09-21 14:34:43 +0200

    Update verify endpoint and tests

--- tests/test_verify.py
+++ tests/test_verify.py
@@ -5,135 +5,58 @@ from verify import app
 CHALLENGE = 'olnkk73345qmdmhlrf6r'
 
 
-def test_lambda_handler(apigw_event):
-    ret = app.handler(apigw_event, "")
-    assert ret["statusCode"] == 200
-    assert ret["body"] == CHALLENGE
+def test_success(success_event):
+    ret = app.handler(success_event, "")
+    assert ret.get('statusCode') == 200
+    assert ret.get('body') == CHALLENGE
 
 
 @pytest.fixture()
-def apigw_event():
+def success_event():
     """ Generates API GW Event"""
 
     return {
-        "resource": "/topic/{id}",
-        "path": "/topic/0019504b-4f69-4ec2-a43e-6eac2537c71d",
-        "httpMethod": "GET",
-        "headers": {
-            "Accept-Encoding": "gzip, deflate",
-            "CloudFront-Forwarded-Proto": "https",
-            "CloudFront-Is-Desktop-Viewer": "true",
-            "CloudFront-Is-Mobile-Viewer": "False",
-            "CloudFront-Is-SmartTV-Viewer": "False",
-            "CloudFront-Is-Tablet-Viewer": "False",
-            "CloudFront-Viewer-Country": "US",
-            "Host": "test1234.execute-api.eu-west-1.amazonaws.com",
-            "User-Agent": "Superfeedr bot/2.0 http://superfeedr.com",
-            "Via": "1.1 056e8663365bf1daf840591db57b77b6.cloudfront.net",
-            "X-Amz-Cf-Id": "4d_8R-dfdsfd==",
-            "X-Amzn-Trace-Id": "Root=1-5ba0b84a-fe3cea0060088fdc7570f446",
-            "X-Forwarded-For": "198.58.102.96, 52.46.21.72",
-            "X-Forwarded-Port": "443",
-            "X-Forwarded-Proto": "https"
-        },
-        "multiValueHeaders": {
-            "Accept-Encoding": [
-                "gzip, deflate"
-            ],
-            "CloudFront-Forwarded-Proto": [
-                "https"
-            ],
-            "CloudFront-Is-Desktop-Viewer": [
-                "true"
-            ],
-            "CloudFront-Is-Mobile-Viewer": [
-                "False"
-            ],
-            "CloudFront-Is-SmartTV-Viewer": [
-                "False"
-            ],
-            "CloudFront-Is-Tablet-Viewer": [
-                "False"
-            ],
-            "CloudFront-Viewer-Country": [
-                "US"
-            ],
-            "Host": [
-                "test1234.execute-api.eu-west-1.amazonaws.com"
-            ],
-            "User-Agent": [
-                "Superfeedr bot/2.0 http://superfeedr.com"
-            ],
-            "Via": [
-                "1.1 056e8663365bf1daf840591db57b77b6.cloudfront.net"
-            ],
-            "X-Amz-Cf-Id": [
-                "4d_8R-dfdsfd=="
-            ],
-            "X-Amzn-Trace-Id": [
-                "Root=1-5ba0b84a-fe3cea0060088fdc7570f446"
-            ],
-            "X-Forwarded-For": [
-                "198.58.102.96, 52.46.21.72"
-            ],
-            "X-Forwarded-Port": [
-                "443"
-            ],
-            "X-Forwarded-Proto": [
-                "https"
-            ]
+        'pathParameters': {
+            'id': '1956576f-c82a-49d3-b23e-0eb9c5dde718'
         },
         "queryStringParameters": {
             "hub.challenge": CHALLENGE,
             "hub.lease_seconds": "315360000",
             "hub.mode": "subscribe",
             "hub.topic": "https://daringfireball.net/feeds/main"
-        },
-        "multiValueQueryStringParameters": {
-            "hub.challenge": [
-                CHALLENGE
-            ],
-            "hub.lease_seconds": [
-                "315360000"
-            ],
-            "hub.mode": [
-                "subscribe"
-            ],
-            "hub.topic": [
-                "https://daringfireball.net/feeds/main"
-            ]
-        },
-        "pathParameters": {
-            "id": "0019504b-4f69-4ec2-a43e-6eac2537c71d"
-        },
-        "stageVariables": "None",
-        "requestContext": {
-            "resourceId": "flx4in",
-            "resourcePath": "/topic/{id}",
-            "httpMethod": "GET",
-            "extendedRequestId": "NaG7oG1AjoEFgTQ=",
-            "requestTime": "18/Sep/2018:08:33:14 +0000",
-            "path": "/Prod/topic/94deac4e-7887-4986-ae7f-a24db8051591",
-            "accountId": "200885815221",
-            "protocol": "HTTP/1.1",
-            "stage": "Prod",
-            "requestTimeEpoch": 1537259594407,
-            "requestId": "7c06dbd9-bb1d-11e8-99b1-cb67dd6d12de",
-            "identity": {
-                "cognitoIdentityPoolId": "None",
-                "accountId": "None",
-                "cognitoIdentityId": "None",
-                "caller": "None",
-                "sourceIp": "198.58.102.96",
-                "accessKey": "None",
-                "cognitoAuthenticationType": "None",
-                "cognitoAuthenticationProvider": "None",
-                "userArn": "None",
-                "userAgent": "Superfeedr bot/2.0 http://superfeedr.com",
-                "user": "None"
-            },
-            "apiId": "auyek5vk39"
-        },
-        "body": "None",
-        "isBase64Encoded": "False"
+        }
+    }
+
+
+def test_no_challenge(no_challenge_event):
+    ret = app.handler(no_challenge_event, "")
+    assert ret.get('statusCode') == 400
+    assert ret.get('body') is None
+
+
+@pytest.fixture()
+def no_challenge_event():
+    """ Generates API GW Event"""
+
+    return {
+        'pathParameters': {
+            'id': '1956576f-c82a-49d3-b23e-0eb9c5dde718'
+        }
+    }
+
+
+def test_wrong_topic_id(wrong_topic_id_event):
+    ret = app.handler(wrong_topic_id_event, "")
+    assert ret.get('statusCode') == 400
+    assert ret.get('body') is None
+
+
+@pytest.fixture()
+def wrong_topic_id_event():
+    """ Generates API GW Event"""
+
+    return {
+        'pathParameters': {
+            'id': '2be193d3-96f7-4bea-a48d-e8edc21b2731'
+        }
     }
--- verify/app.py
+++ verify/app.py
@@ -1,21 +1,29 @@
+import boto3
 import logging
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+TOPIC = "macnerd-topic"
+
+
+def respond(code=200, body=None):
+    return {
+        'statusCode': code,
+        'body': body
+    }
+
 
 def handler(event, context):
     logging.info(event)
-    if not event.get('queryStringParameters'):
-        return {
-            'statusCode': 400
-        }
-    if not event.get('queryStringParameters').get('hub.challenge'):
-        return {
-            'statusCode': 400
-        }
 
-    return {
-        'statusCode': 200,
-        'body': event.get('queryStringParameters').get('hub.challenge')
-    }
+    challenge = event.get('queryStringParameters', {}).get('hub.challenge')
+    if not challenge:
+        return respond(400)
+
+    table = boto3.resource('dynamodb').Table(TOPIC)
+    topic_id = event.get('pathParameters', {}).get('id')
+    if not table.get_item(Key={'id': topic_id}).get('Item'):
+        return respond(400)
+
+    return respond(200, challenge)
